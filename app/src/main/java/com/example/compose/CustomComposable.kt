dc0da04151a3b24ad66ad30b5df869fd8afde39d
lh <im.lh@hotmail.com>
2021-07-06 15:46:27 +0800

    fix error

--- app/src/main/java/com/example/compose/CustomComposable.kt
+++ app/src/main/java/com/example/compose/CustomComposable.kt
@@ -1,7 +1,11 @@
 package com.example.compose
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 /**
  * 自定义Composable
